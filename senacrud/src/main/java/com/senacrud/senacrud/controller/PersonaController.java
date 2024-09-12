bda68b3792325116d9888a46a02f7e84d01f4e13
Jeison <j.stanleyz.c0619@gmail.com>
2024-09-11 23:02:51 -0500

    se crea controller getbyid

--- senacrud/src/main/java/com/senacrud/senacrud/controller/PersonaController.java
+++ senacrud/src/main/java/com/senacrud/senacrud/controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.senacrud.senacrud.models.Persona;
 import com.senacrud.senacrud.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +18,10 @@ public class PersonaController {
     @GetMapping("personas")
     public List<Persona> getAll(){
         return personaService.findAll();
+    }
+
+    @GetMapping("personas/{id}") //lo que esta entre llaves lo captura dinamicamente con @PathVaribale
+    public Persona getById(@PathVariable Long id){
+        return personaService.getById(id);
     }
 }
